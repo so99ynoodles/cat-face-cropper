b66be5b1fd0b505cf30cb83f690d74594f28c064
so99ynoodles <so99ynoodles@gmail.com>
2021-02-06 14:28:53 +0900

    Add models

--- main.py
+++ main.py
@@ -17,15 +17,23 @@ new_size = (width, height)
 if not os.path.isdir(export_folder):
     os.mkdir(export_folder)
 
-catface_cascade = cv2.CascadeClassifier('./catface_detector.xml')
+# catface_cascade = cv2.CascadeClassifier('./models/catface_detector.xml')
+# catface_cascade = cv2.CascadeClassifier('./modles/haarcascade_frontalcatface.xml')
+catface_cascade = cv2.CascadeClassifier('./models/haarcascade_frontalcatface_extended.xml')
+
 pictures = list(filter(lambda x: x.endswith(extensions), os.listdir(import_folder)))
+
+total = 0
 
 for pic_path in pictures:
     image_name = os.path.splitext(pic_path)[0]
     image = cv2.imread(f'{import_folder}/{pic_path}')
-    cat_faces = catface_cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=3, minSize=(75, 75))
+    cat_faces = catface_cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=1, minSize=(15, 15))
     print(f'{len(cat_faces)} faces detected')
+    total += len(cat_faces)
     for (i, (x, y, w, h)) in enumerate(cat_faces):
-        face = image[y-50:y+h+50, x-50:x+w+50]
+        face = image[y:y+h, x:x+w]
         resized = cv2.resize(face, (width, height))
         cv2.imwrite(f'{export_folder}/{image_name}.png', resized)
+
+print(f'{total} cat faces detected with {len(pictures)} pictures.')
